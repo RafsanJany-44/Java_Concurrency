81d56b39f82266ff3373296f15c1bbebae5b91df
Rafsan Jany, 170042044 <59754354+RafsanJany-44@users.noreply.github.com>
2021-10-18 02:22:24 +0600

    hello

--- threads/chopper.java
+++ threads/chopper.java
@@ -1,7 +1,6 @@
 package threads;
 
-public class Task_1 extends Thread{
-
+public class chopper extends Thread {
     public void run(){
         for(int i=1;i<=10;i++){
             System.out.println("Task 1 continue: "+i);
@@ -10,5 +9,4 @@ public class Task_1 extends Thread{
             }catch(Exception e){}
         }
         System.out.println("Task 1 Completed");
-    }
 }
